609c970e8a11eec0260f5b6298b2ee72de634fe7
shelbyd <shelbyd@localhost>
2015-08-18 13:10:01 -0700

    finish Nodes::Source

--- lib/nodes/source.rb
+++ lib/nodes/source.rb
@@ -1,6 +1,39 @@
+require 'json'
+
 module Nodes
   class Source < Struct.new(:data_dir_path)
     def next
+      @index = @index ? @index + 1 : 0
+      objects[@index] || reset_and_try_again
+    end
+
+    private
+
+    def reset_and_try_again
+      @index = nil
+      @current_objects = nil
+      @file_index += 1
+      self.next if current_file_exists?
+    end
+
+    def current_file_exists?
+      File.exist?(current_file_path)
+    end
+
+    def current_file_path
+      @file_index ||= 0
+      "#{data_dir_path}/data/#{@file_index}.data"
+    end
+
+    def objects
+      @current_objects ||= begin
+        if current_file_exists?
+          File.readlines(current_file_path)
+            .map { |line| JSON.parse line }
+        else
+          []
+        end
+      end
     end
   end
 end
--- spec/lib/nodes/source_spec.rb
+++ spec/lib/nodes/source_spec.rb
@@ -1,18 +1,23 @@
 require 'nodes/source'
-require 'json'
 
 describe Nodes::Source do
   let(:objects) { [] }
   let(:file_contents) { objects.map(&:to_json).join("\n") }
-  let(:data_dir) { './data' }
+  let(:data_dir) { './data/test' }
+  let(:raw_data_dir) { "#{data_dir}/data" }
 
-  before { Dir.mkdir(data_dir) unless File.directory?(data_dir) }
-  before { File.open("#{data_dir}/data.data", 'w') { |f| f.write(file_contents) } }
+  after { FileUtils.rmtree(data_dir) }
+
+  before { FileUtils.mkdir_p(raw_data_dir) }
+  before { File.open("#{raw_data_dir}/0.data", 'w') { |f| f.write(file_contents) } }
 
   describe '#next' do
-    subject { described_class.new(data_dir).next }
+    let(:node) { described_class.new(data_dir) }
+    let(:call_count) { 0 }
+    subject { call_count.times { node.next }; node.next }
 
     context 'with no data' do
+      before { FileUtils.rmtree(data_dir) }
       it { is_expected.to eq nil }
     end
 
@@ -20,6 +25,85 @@ describe Nodes::Source do
       let(:objects) { [{'foo' => 'bar'}] }
 
       it { is_expected.to eq objects[0] }
+
+      context 'after being called once' do
+        let(:call_count) { 1 }
+
+        it { is_expected.to be_nil }
+      end
+    end
+
+    context 'with three objects' do
+      let(:objects) { [
+        {'foo' => 'foo'},
+        {'bar' => 'bar'},
+        {'baz' => 'baz'},
+      ] }
+
+      it { is_expected.to eq objects[0] }
+
+      context 'after being called once' do
+        let(:call_count) { 1 }
+        it { is_expected.to eq objects[1] }
+      end
+
+      context 'after being called twice' do
+        let(:call_count) { 2 }
+        it { is_expected.to eq objects[2] }
+      end
+
+      context 'after being called three times' do
+        let(:call_count) { 3 }
+        it { is_expected.to be_nil }
+      end
+    end
+
+    context 'with data split up across three files' do
+      let(:objects0) { [
+        {'foo0' => 'foo0'},
+        {'bar0' => 'bar0'},
+        {'baz0' => 'baz0'},
+      ] }
+      let(:objects1) { [
+        {'foo1' => 'foo1'},
+        {'bar1' => 'bar1'},
+        {'baz1' => 'baz1'},
+      ] }
+      let(:objects2) { [
+        {'foo2' => 'foo2'},
+        {'bar2' => 'bar2'},
+        {'baz2' => 'baz2'},
+      ] }
+
+      before do
+        [objects0, objects1, objects2].each_with_index do |objects, index|
+          File.open("#{raw_data_dir}/#{index}.data", 'w') do |f|
+            f.write objects.map(&:to_json).join("\n")
+          end
+        end
+      end
+
+      it { is_expected.to eq objects0[0] }
+
+      context 'after being called three times' do
+        let(:call_count) { 3 }
+        it { is_expected.to eq objects1[0] }
+      end
+
+      context 'after being called six times' do
+        let(:call_count) { 6 }
+        it { is_expected.to eq objects2[0] }
+      end
+
+      context 'after being called eight times' do
+        let(:call_count) { 8 }
+        it { is_expected.to eq objects2[2] }
+      end
+
+      context 'after being called nine times' do
+        let(:call_count) { 9 }
+        it { is_expected.to be_nil }
+      end
     end
   end
 end
